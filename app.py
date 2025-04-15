6ca5e5dc4404b1cf7405e3cc46c76725b64b9318
Manthar ALi <mantharmanthar437@gmail.com>
2025-04-15 12:50:49 +0500

    message

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ with open("model.pkl", 'rb') as file:
     model = pickle.load(file)
 
 user_input =st.number_input("Experience Years",min_value=0.0,max_value=50.0,value=0.0,step=0.1)
-st.success(model.predict([[user_input]]))
+if st.button("predict Salary"):
+ st.success(model.predict([[user_input]]))
