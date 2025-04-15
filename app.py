80bf094350bf43153b4c2a203633c2938d65b240
Manthar ALi <mantharmanthar437@gmail.com>
2025-04-15 12:15:15 +0500

    set path

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 import streamlit as st
 import pickle
-import os
 
 st.title("Salary Predictor")
 
-# Function to resolve the model path
-def get_model_path():
-    # Check if running on Streamlit Cloud
-    if "STREAMLIT_CLOUD" in os.environ:
-        # Path for Streamlit Cloud
-        return "../model.pkl"
-    else:
-        # Path for local environment
-        return os.path.join(os.path.dirname(__file__), "model.pkl")
 
-
-with open(get_model_path(), 'rb') as file:
+with open("model.pkl", 'rb') as file:
     model = pickle.load(file)
 
 user_input =st.number_input("Experience Years",min_value=0.0,max_value=50.0,value=0.0,step=0.1)
